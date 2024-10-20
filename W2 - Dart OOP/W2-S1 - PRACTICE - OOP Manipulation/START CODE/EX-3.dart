25e83860492f5bb6cb00e7299b7d06dbd71f0273
iamkaizen <kimsokhom654@gmail.com>
2024-10-21 01:02:55 +0700

    Finishes ex3

--- W2 - Dart OOP/W2-S1 - PRACTICE - OOP Manipulation/START CODE/EX-3.dart
+++ W2 - Dart OOP/W2-S1 - PRACTICE - OOP Manipulation/START CODE/EX-3.dart
@@ -1,27 +1,53 @@
 class MyDuration {
-  
+  final int _milliseconds;
 
-//   // Display the duration in a readable format
-//   @override
-//   String toString() {
-//     int seconds = (_milliseconds / 1000).round();
-//     int minutes = (seconds / 60).floor();
-//     seconds = seconds % 60;
-//     int hours = (minutes / 60).floor();
-//     minutes = minutes % 60;
-//     return '$hours hours, $minutes minutes, $seconds seconds';
-//   }
-// }
+  MyDuration(this._milliseconds)
+      : assert(_milliseconds >= 0,
+            "Duration shall always be greater or equal to 0!");
+  factory MyDuration.fromHours(int hours) => MyDuration(hours * 3600000);
+  factory MyDuration.fromMinutes(int minutes) => MyDuration(minutes * 60000);
+  factory MyDuration.fromSeconds(int seconds) => MyDuration(seconds * 1000);
 
-// void main() {
-//   MyDuration duration1 = MyDuration.fromHours(3); // 3 hours
-//   MyDuration duration2 = MyDuration.fromMinutes(45); // 45 minutes
-//   print(duration1 + duration2); // 3 hours, 45 minutes, 0 seconds
-//   print(duration1>duration2); //true
+  int get getMillisecond => _milliseconds;
 
-//   try {
-//     print(duration2 - duration1); // This will throw an exception
-//   } catch (e) {
-//     print(e); 
-//   }
+  bool operator >(MyDuration other) {
+    return this._milliseconds > other._milliseconds;
+  }
+
+  MyDuration operator +(MyDuration other) {
+    return MyDuration(this._milliseconds + other._milliseconds);
+  }
+
+  MyDuration operator -(MyDuration other) {
+    int result = this._milliseconds - other._milliseconds;
+    if (result < 0) {
+      throw Exception('Resulting duration cannot be negative');
+    }
+    return MyDuration(result);
+  }
+
+  // Display the duration in a readable format
+  @override
+  String toString() {
+    int seconds = (_milliseconds / 1000).round();
+    int minutes = (seconds / 60).floor();
+    seconds = seconds % 60;
+    int hours = (minutes / 60).floor();
+    minutes = minutes % 60;
+    return '$hours hours, $minutes minutes, $seconds seconds';
+  }
+}
+
+
+void main() {
+  MyDuration duration1 = MyDuration.fromHours(3); // 3 hours
+  MyDuration duration2 = MyDuration.fromMinutes(45); // 45 minutes
+  print(duration1 + duration2); // 3 hours, 45 minutes, 0 seconds
+  print(duration1 > duration2); //true
+
+  try {
+    print(duration2 - duration1); // This will throw an exception
+  } catch (e) {
+    print(e);
+  }
 }
